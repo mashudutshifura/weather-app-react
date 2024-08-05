cc448f0aec11f318305d257698d811077ed59dab
mashudutshifura <164932719+mashudutshifura@users.noreply.github.com>
2024-08-05 22:41:00 +0200

    deleted the search function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./styles.css"
 import "./weather.css"
-import Search from "./Search";
 import Weather from "./Weather";
 import Footer from "./Footer";
 
@@ -9,7 +8,6 @@ import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
-      <Search />
       <Weather />
       <Footer />
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,30 +1,68 @@
 import React from "react";
-import "./weather.css";
+import "./Weather.css";
 
 export default function Weather() {
+  let weatherData = {
+    city: "Paris",
+    temperature: 15,
+    date: "Monday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
+
   return (
     <div className="Weather">
-      <div>
-        <h1 class="weather-city">Pretoria</h1>
-        <p class="weather-details">
-          <span id="time">Monday</span> <span id="description"></span>
-          <br />
-          Humidity: <strong id="humidity">73%</strong>, Wind:
-          <strong id="wind-speed">3.09km/h</strong>
-        </p>
-      </div>
-      <div className="weather-temperature-container">
+      <form className="mb-3">
         <div className="row">
-          <div className="col-6">
-            <div className="weather-icon" id="icon"></div>
-            <div className="temperature-value" id="temperature">
-              17
-            </div>
-            <div className="temperature-unit">°C</div>
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
       </div>
-      <div className="weather-forecast" id="forecast"></div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
